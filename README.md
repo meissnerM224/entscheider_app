0944bfedd6ba792f0fcb97eb9491ac87e16143b8
meissnerM224 <meissnermw@gmail.com>
2024-02-23 22:42:28 +0100

    init entscheider

---
+++ README.md
@@ -0,0 +1,3 @@
+# entscheider_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/entscheider_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.entscheider_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
